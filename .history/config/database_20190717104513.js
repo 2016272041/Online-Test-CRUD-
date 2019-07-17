6e413f0e76f54812ed0089d6c5a862bc78930684
SHIVA <balachandarsivakumar@gmail.com>
2019-07-17 18:37:15 +0530

    Source update

---
+++ .history/config/database_20190717104513.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'root',
+        'password': 'Sqladmin_1'
+    },
+	'database': 'test',
+    'users_table': 'user'
+};
